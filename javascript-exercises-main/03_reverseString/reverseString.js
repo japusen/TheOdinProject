41fa7fdbb47271421778cfd563f7af70055e3ebf
Jay <japusen32@gmail.com>
2023-10-03 16:51:36 -0700

    Completed exercise 3

--- javascript-exercises-main/03_reverseString/reverseString.js
+++ javascript-exercises-main/03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reversedArray = stringArray.reverse();
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
