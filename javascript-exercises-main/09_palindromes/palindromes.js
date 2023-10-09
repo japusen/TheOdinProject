796db407ae71fc7c1791eb1db7869eca955b95bf
Jay <japusen32@gmail.com>
2023-10-09 16:16:32 -0700

    Completed exercise 09

--- javascript-exercises-main/09_palindromes/palindromes.js
+++ javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    let removeCapsAndPunc = words.toLowerCase().replace(/\W|_/g, '');
+    let array = Array.from(removeCapsAndPunc);
+    let reverseArray = Array.from(array).reverse();
+    return JSON.stringify(array) === JSON.stringify(reverseArray);
 };
 
 // Do not edit below this line
