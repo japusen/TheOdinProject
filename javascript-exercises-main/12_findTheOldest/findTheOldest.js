dd0c0449dbdbdbb92e8f55f1d5e7961b974204bb
Jay <japusen32@gmail.com>
2023-10-09 21:04:09 -0700

    Completed exercise 12

--- javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => older(oldest, person), people[0]);
 };
+
+function older(firstPerson, secondPerson) {
+    return age(firstPerson) > age(secondPerson) ? firstPerson : secondPerson;
+}
+
+function age(person) {
+    return ('yearOfDeath' in person) ? 
+    person.yearOfDeath - person.yearOfBirth : 
+    new Date().getFullYear() - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
