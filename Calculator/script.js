0592fa415721f884498213f73b17ffc1e3e69f43
Jay <japusen32@gmail.com>
2023-10-10 21:25:25 -0700

    Created functions for button functionality and button click events

--- Calculator/script.js
+++ Calculator/script.js
@@ -31,6 +31,63 @@ function operate(operator, firstOperand, secondOperand) {
     }
 }
 
+function clearDisplay() {
+    if (displayNumber.length !== 0) {
+        updateDisplayNumber('');
+        updateDisplayExpression('');
+    }
+}
+
+function deleteDisplayNumber() {
+    if (displayNumber.length !== 0) {
+        updateDisplayNumber(displayNumber.slice(0, -1))
+    }
+}
+
+function updateDisplayNumber(number) {
+    displayNumber = number;
+    let display = document.querySelector('.current-operand');
+    display.textContent = displayNumber;
+}
+
+function updateDisplayExpression(expression) {
+    previousExpression = expression;
+    let displayExpression = document.querySelector('.previous-operation');
+    displayExpression.textContent = previousExpression;
+}
+
+function initButtons() {
+    let clearButton = document.querySelector('.clear-btn');
+    clearButton.addEventListener('click', () => {
+        clearDisplay();
+    });
+
+    let deleteButton = document.querySelector('.delete-btn');
+    deleteButton.addEventListener('click', deleteDisplayNumber);
+
+    const buttonsMap = new Map([
+        ['zero-btn', 0], ['one-btn', 1], ['two-btn', 2],
+        ['three-btn', 3], ['four-btn', 4], ['five-btn', 5],
+        ['six-btn', 6], ['seven-btn', 7], ['eight-btn', 8], ['nine-btn', 9]
+    ]);
+    
+    buttonsMap.forEach((value, key, map) => {
+        let currentButton = document.querySelector(`button.${key}`);
+        currentButton.addEventListener('click', () => {
+            let newNumber = displayNumber.concat(value);
+            updateDisplayNumber(newNumber);
+        });
+    });
+}
+
+
+
 let firstOperand = '';
 let secondOperand = '';
 let operator = null;
+let previousExpression = '';
+let displayNumber = '';
+
+initButtons();
+updateDisplayNumber('0');
+updateDisplayExpression('0 + 0');
