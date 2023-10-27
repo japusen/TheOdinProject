ba668ed46ecb49aa63901d5c39bf958787a69fda
Jay <japusen32@gmail.com>
2023-10-26 22:31:23 -0700

    Webpack tutorial

---
+++ webpack-tutorial/src/index.js
@@ -0,0 +1,11 @@
+import myName from "./myName";
+
+function component() {
+    const element = document.createElement('div');
+
+    element.textContent = myName('Cody');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
