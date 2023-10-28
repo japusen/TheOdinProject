5e025d312299b179f6794e99a8a53caec9bd2f3f
Jay <japusen32@gmail.com>
2023-10-27 22:29:56 -0700

    Implemented basic contact tab

--- Restaurant-Page/src/contact.js
+++ Restaurant-Page/src/contact.js
@@ -1,7 +1,17 @@
 const contact = () => {
     let content = document.querySelector('div.content');
-    
 
+    let contact = document.createElement('div');
+    content.appendChild(contact);
+    contact.classList.add('contact-info');
+
+    let address = document.createElement('div');
+    contact.appendChild(address);
+    address.textContent = 'Address: 123 Main Street, City, State';
+
+    let phone = document.createElement('div');
+    contact.appendChild(phone);
+    phone.textContent = 'Phone: (555) 666 - 7777';
 }
 
 export { contact };
